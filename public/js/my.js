752c87c780c245dd63cbbdbd36e17a8084f45c8d
Roman-Liubinets <roman.liubinets@gmail.com>
2018-04-12 19:59:29 +0300

    first commit

---
+++ public/js/my.js
@@ -0,0 +1,47 @@
+//Під`єднюємо ангуляр
+var app = angular.module('app', ['ngRoute']);
+
+//Забираєм %2F та # з url сайту
+app.config(['$locationProvider', function ($locationProvider) {
+    $locationProvider.hashPrefix('');
+    $locationProvider.html5Mode(true);
+}]);
+
+//Створюєм адреси
+app.config(function ($routeProvider) {
+    $routeProvider
+        .otherwise({
+            redirectTo: '/'
+        });
+
+});
+
+//Створюємо контроллер
+app.controller('myCtrl', function ($scope, $http) {});
+
+//Директиви
+app.directive("headerBlock", function () {
+    return {
+        replace: true,
+        templateUrl: "template/header.html",
+        controller: function ($scope, $http) {
+          $scope.menuNavBtn = [{
+              
+          }]  
+        }
+    }
+});
+
+app.directive("bodyBlock", function () {
+    return {
+        replace: true,
+        templateUrl: "template/body.html",
+        controller: function ($scope, $http) {}
+    }
+});
+
+app.directive("footerBlock", function () {
+    return {
+        replace: true,
+        templateUrl: "template/footer.html",
+        c
